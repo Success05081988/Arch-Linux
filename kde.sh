31125e2dcb89d11224c57fb4a28882e7c6f50f37
Success <121008519+Success05081988@users.noreply.github.com>
2023-02-02 23:39:50 +0500

    Add files via upload

---
+++ kde.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+sudo timedatectl set-ntp true
+sudo hwclock --systohc
+
+sudo reflector -c Germany -a 6 --sort rate --save /etc/pacman.d/mirrorlist
+
+#sudo firewall-cmd --add-port=1025-65535/tcp --permanent
+#sudo firewall-cmd --add-port=1025-65535/udp --permanent
+#sudo firewall-cmd --reload
+
+git clone https://aur.archlinux.org/yay.git
+cd yay/
+makepkg -si --noconfirm
+
+#pikaur -S --noconfirm system76-power
+#sudo systemctl enable --now system76-power
+#sudo system76-power graphics integrated
+#pikaur -S --noconfirm auto-cpufreq
+#sudo systemctl enable --now auto-cpufreq
+
+
+sudo pacman -S --noconfirm xorg-server xorg-xinit xorg-xkill xterm sddm vlc materia-kde ark dolphin konsole kwrite plasma-meta yakuake
+
+#sudo flatpak install -y spotify
+
+sudo systemctl enable sddm
+/bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
+sleep 5
+reboot
