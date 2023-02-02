21deb04499d3fc2f8d560357f9f5ae046afcc77c
Success <121008519+Success05081988@users.noreply.github.com>
2023-02-03 01:47:54 +0500

    Update media.sh

--- media.sh
+++ media.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pacman -S --noconfirm alsa-card-profiles alsa-lib alsa-plugins alsa-firmware alsa-utils gst-libav gst-plugin-pipewire gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gstreamer gstreamer-vaapi libpulse pipewire pipewire-alsa pipewire-jack pipewire-pulse wireplumber x264 x265 xvidcore
+pacman -S --noconfirm alsa-card-profiles alsa-lib alsa-plugins alsa-firmware alsa-utils gst-libav gst-plugin-pipewire gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly libpulse pipewire pipewire-alsa pipewire-jack pipewire-pulse wireplumber x264 x265 xvidcore
 
 
 /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
