f66ac82263984d583d3724e53d746ca990251b4b
Success <121008519+Success05081988@users.noreply.github.com>
2023-02-03 19:06:08 +0500

    Update base-mbr.sh

--- base-mbr.sh
+++ base-mbr.sh
@@ -12,7 +12,7 @@ echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 echo root:password | chpasswd
-pacman -S reflector
+pacman -S --noconfirm reflector
 reflector --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
